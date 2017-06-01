0e228f71c2bc13b4a60fead76703cd71bc582da4
Mohammad <mohammad.altahan@sharedop.com>
2017-06-01 14:50:30 +0200

    Creating Logging the problem was you have to set the log4net copy to output directory to copy always, because by default the config file dont added automaticlly to the bin folder. and you should not copy app.config from app to another because they are different(console,web,..). for app.config it convert to dll file you can find it in the bin folder.

--- TestLogging/Program.cs
+++ TestLogging/Program.cs
@@ -12,6 +12,7 @@ namespace TestLogging
     {
         static void Main(string[] args)
         {
+            
             var traceSource = Logger.Get(TestLogging.Logging.Logging.TraceSourceName.TEST_LOGGING);
 
             traceSource.TraceEvent(TraceEventType.Start, (int)TestLogging.Logging.Logging.EventId.Start, "Started");
