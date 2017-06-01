adcd4eb68f1e4df17f5ee6c51e94755cdb7c6f63
Mohammad <mohammad.altahan@sharedop.com>
2017-06-01 14:20:07 +0200

    Add project files.

---
+++ TestLogging/Logging/Logging.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLogging.Logging
+{
+    class Logging
+    {
+        public enum EventId
+        {
+            Default = 0,
+
+            Stop = int.MaxValue - 2,
+            Start = int.MaxValue - 1,
+
+            Exception = int.MaxValue
+        }
+
+        public static class TraceSourceName
+        {
+            public const string TEST_LOGGING = "TestLogging";
+        }
+    }
+}
---
+++ TestLogging/Program.cs
@@ -0,0 +1,22 @@
+﻿using biz.dfch.CS.Commons.Diagnostics;
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+
+
+namespace TestLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var traceSource = Logger.Get(TestLogging.Logging.Logging.TraceSourceName.TEST_LOGGING);
+
+            traceSource.TraceEvent(TraceEventType.Start, (int)TestLogging.Logging.Logging.EventId.Start, "Started");
+
+            traceSource.TraceEvent(TraceEventType.Start, (int)TestLogging.Logging.Logging.EventId.Start, "SUCCEEDED");
+        }
+    }
+}
