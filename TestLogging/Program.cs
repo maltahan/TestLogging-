4e9aae59b3d20af33d397fc0aeb8e7264292229a
Mohammad <mohammad.altahan@sharedop.com>
2017-06-01 15:29:44 +0200

    activiate LogFileAppender appender.

--- TestLogging/Program.cs
+++ TestLogging/Program.cs
@@ -12,7 +12,7 @@ namespace TestLogging
     {
         static void Main(string[] args)
         {
-            
+            //this is the Develop branch
             var traceSource = Logger.Get(TestLogging.Logging.Logging.TraceSourceName.TEST_LOGGING);
 
             traceSource.TraceEvent(TraceEventType.Start, (int)TestLogging.Logging.Logging.EventId.Start, "Started");
